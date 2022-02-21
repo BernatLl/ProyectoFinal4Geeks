5f17b4602d60699ecf4c7eda80340d0de0d8d9dc
BernatLl <blloret@hotmail.com>
2022-02-21 13:14:28 +0000

    up

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -27,19 +27,14 @@ export const Footer = () => (
 							<li>Contact Us</li>
 							<li>About Us</li>
 							<li>Career</li>
-							<li>Download App</li>
-							<li>Help and Support</li>
-							<li>Blog</li>
-							<li>Forum</li>
+							
 						</ul>
 					</div>
 					<div className="col">
 						<ul>
 							<li>Terms</li>
 							<li>Privacy Policy</li>
-							<li>Cookies Settings</li>
-							<li>Site Map</li>
-							<li>Accessibility</li>
+							
 							
 						</ul>
 					</div>
