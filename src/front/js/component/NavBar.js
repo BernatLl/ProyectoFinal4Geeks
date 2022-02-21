0b69706e9cfb4065b7c6cbbf17c01fe4cf5d81b3
leotranq <leotranq@gmail.com>
2022-02-21 13:39:13 +0000

    update

--- src/front/js/component/NavBar.js
+++ src/front/js/component/NavBar.js
@@ -4,17 +4,37 @@ import logo from "../../img/NavEdit.png";
 export const Navbar = () => {
   return (
     <div className="Back">
-      <nav id="NavBack" className="nav nav-pills flex-column flex-sm-row">
+      <nav id="NavBack" className="nav nav-pills flex-column flex-md-row">
         <a id="logo" className="flex-sm-fill text-sm-center nav-link" href="#">
           <img className="logo" src={logo}></img>
         </a>
         <a
-          id="categories"
-          className="flex-sm-fill text-sm-center nav-link"
+          class="nav-link dropdown-toggle"
           href="#"
+          id="categories"
+          role="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
         >
           Categories
         </a>
+        <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+          <li>
+            <a class="dropdown-item" href="#">
+              Chef
+            </a>
+          </li>
+          <li>
+            <a class="dropdown-item" href="#">
+              Style
+            </a>
+          </li>
+          <li>
+            <a class="dropdown-item" href="#">
+              Country
+            </a>
+          </li>
+        </ul>
         <a
           id="student"
           className="flex-sm-fill text-sm-center nav-link"
@@ -30,13 +50,22 @@ export const Navbar = () => {
           Professor
         </a>
 
-        <a
-          id="search"
-          className="flex-sm-fill text-sm-center nav-link "
-          href="#"
-        >
-          Search
-        </a>
+        <form id="SearchBar" className="d-flex">
+          <input
+            className="form-control me-2"
+            type="search"
+            id="Search"
+            placeholder="Type Here"
+            aria-label="Search"
+          ></input>
+          <button
+            id="SButton"
+            type="button"
+            className="btn btn-secondary btn-sm"
+          >
+            Search
+          </button>
+        </form>
 
         <button id="cart" type="button" className="btn btn-secondary btn-sm">
           Cart
