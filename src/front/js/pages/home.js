6248416d26294d0d6d668eb14132b230adbc00b1
leotranq <leotranq@gmail.com>
2022-02-21 17:53:14 +0000

    update finish

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -67,13 +67,41 @@ export const Home = () => {
             ></img>
 
             <div className="StudentsText">
-              Welcome to "We Cook", with us you will be able to access to a
-              digital library with hundreds of professional cooks around the
-              world, do you want to learn how to cut like a professional Click
-              here, Do you want to learn about Venezuelan food, New Zeleand,
-              Canada, Scotland, etc? We give the chance to professional or no
-              professional chefs around the world that want to share for a
-              modest price their recipe or skill with the rest of the world!!!!{" "}
+              <h6>
+                {" "}
+                <h5>Students</h5>
+                Welcome to "We Cook", with us you will be able to access to a
+                digital library with hundreds of professional cooks around the
+                world, do you want to learn how to cut like a professional Click
+                here, Do you want to learn about Venezuelan food, New Zeleand,
+                Canada, Scotland, etc? We give the chance to professional or no
+                professional chefs around the world that want to share for a
+                modest price their recipe or skill with the rest of the
+                world!!!!{" "}
+              </h6>
+            </div>
+          </div>
+        </div>
+        <div className="ProfessorSide">
+          <div className="Professor">
+            <img
+              className="ProfePic"
+              src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F37%2F2020%2F03%2FOnline-Cooking-Classes.jpg"
+            ></img>
+
+            <div className="ProfeText">
+              <h6>
+                {" "}
+                <h5>Professor</h5>
+                Welcome to "We Cook", with us you will be able to access to a
+                digital library with hundreds of professional cooks around the
+                world, do you want to learn how to cut like a professional Click
+                here, Do you want to learn about Venezuelan food, New Zeleand,
+                Canada, Scotland, etc? We give the chance to professional or no
+                professional chefs around the world that want to share for a
+                modest price their recipe or skill with the rest of the
+                world!!!!{" "}
+              </h6>
             </div>
           </div>
         </div>
