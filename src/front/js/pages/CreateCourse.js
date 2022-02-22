398adc1dbfab989c2964479130e0e04a3b9b7d21
leotranq <leotranq@gmail.com>
2022-02-22 18:26:31 +0000

    update

--- src/front/js/pages/CreateCourse.js
+++ src/front/js/pages/CreateCourse.js
@@ -1,39 +1,39 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
-
+import "../../styles/CreateCourse.css";
+import ClaseRemote from "../../img/claseRemote.jpg";
 export const CreateCourse = () => {
   return (
     <div className="BackGround">
-      <div className="Tittle">
-        <h1>Educator Center</h1>
-      </div>
-      <div className="Slogan">
-        <h2>
-          Better than a thousand days of diligent study, is one day with a great
-          teacher.
-        </h2>
-      </div>
-      <div>Create a Course</div>
-      <div className="Recomendations">
-        <Card style={{ width: "18rem" }}>
-          <Card.Img
-            variant="top"
-            src="https://thumbs.dreamstime.com/b/check-resume-concept-resume-hands-check-resume-concept-resume-hands-approval-resume-flat-style-147230943.jpg"
-          />
-          <Card.Body>
-            <Card.Title>Curriculum Vitae</Card.Title>
-            <Card.Text>
-              One of the best features of this platform is the promotion of the
-              Chef, Adding a Good CV will give confidence to the student at the
-              time of deciding if spend the money on you or another Chef. On the
-              other Hand, You will be able to use our CV format for yourself.
-              The profile in We Cook! will give you statistics if needed for you
-              to use for professional purpuses.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
+      <img src={ClaseRemote} className="HeaderCreate"></img>
+      <div className="wrapper">
+        <div className="Tittle">
+          <h3>Educator Center</h3>
+        </div>
+        <div className="Slogan">
+          <h4>
+            Better than a thousand days of diligent study, is one day with a
+            great teacher.
+          </h4>
+          <div className="Recomend">
+            On this page we will got through the basics that we required in
+            order to post your course in our platform. Don’t worry is not
+            complicated:
+            <ul>
+              <li className="List">
+                Create CV, this is the first step and perhaps the most
+                important, here you will describe yourself the best way you can,
+                to sell yourself as a professional just like ion a job
+                interview, here we will create a CV in our format that you will
+                be able to download for your own purpuses. If profile already
+                created you will start in the next step.
+              </li>
+              <li>Tea</li>
+              <li>Milk</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="Recomendations"></div>
       </div>
     </div>
   );
