9c2aa6f3c840250e9034867e3df159331a7f5a43
BernatLl <blloret@hotmail.com>
2022-02-25 14:42:51 +0000

    update

--- src/front/js/pages/payment.js
+++ src/front/js/pages/payment.js
@@ -31,6 +31,23 @@ import {
   const PaymentForm = () => {
     const stripe = useStripe();
     const elements = useElements();
+    const inputStyle = {
+        iconColor: '#c4f0ff',
+        color: 'rgba(217, 100, 89, 1)',
+        fontWeight: '500',
+        fontFamily: 'Roboto, Open Sans, Segoe UI, sans-serif',
+        fontSize: '16px',
+        fontSmoothing: 'antialiased',
+        background: 'black',
+        ':-webkit-autofill': {
+          color: '#fce883',
+        },
+        '::placeholder': {
+          color: '#87BBFD',
+        },
+  }
+
+
     return (
       <>
         <h1>Payment Page</h1>
@@ -38,7 +55,7 @@ import {
             
             <Card.Body>
                 <Card.Title>Payment</Card.Title>
-                <CardElement />
+                <CardElement options={{style: {base: inputStyle,},}}/>
                 
                  <Button variant="dark" onClick={handleSubmit(stripe, elements)}>Buy</Button>{' '}
                 
