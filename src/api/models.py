5fd17620181780415b2e2a6fe2ef213d5addf098
BernatLl <blloret@hotmail.com>
2022-03-05 11:37:19 +0000

    w

--- src/api/models.py
+++ src/api/models.py
@@ -9,22 +9,22 @@ tags = db.Table('tags',
     db.Column('course_id', db.Integer, db.ForeignKey('course.id'),primary_key=True)
 )
 
-# class User(db.Model):
-#     __tablename__ = 'user'
-#     id = db.Column(db.Integer, primary_key=True)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-#     password = db.Column(db.String(80), unique=False, nullable=False)
-#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-#     def __repr__(self):
-#         return f'User {self.id}'
+    def __repr__(self):
+        return f'User {self.id}'
 
-#     def serialize(self):
-#         return {
-#             "id": self.id,
-#             "email": self.email,
-#             # do not serialize the password, its a security breach
-#         }
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
 
  
 
