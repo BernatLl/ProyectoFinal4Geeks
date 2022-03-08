9b2a77798d12ae3a61a867b56e931cf6be173ea0
leotranq <leotranq@gmail.com>
2022-03-08 11:59:50 +0000

    update

--- src/front/js/pages/CourseView.js
+++ src/front/js/pages/CourseView.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "../../styles/CourseView.css";
-import Nav from "react-bootstrap/Nav";
 import Tabs from "react-bootstrap/Tabs";
 import Tab from "react-bootstrap/Tab";
 export const CourseView = () => {
@@ -12,10 +11,10 @@ export const CourseView = () => {
         </div>
         <div className="VideoWrapper">
           <iframe
-            className="VideoPlayer"
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/X_qo3lnRS1k"
+            className="videoplayer"
+            width="100%"
+            height="1000"
+            src="https://www.youtube.com/embed/qSHXG-5ShFk"
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -31,50 +30,93 @@ export const CourseView = () => {
           className="mb-3"
         >
           <Tab tabClassName="TabTab" eventKey="home" title="Content">
-            <h6 className="TabTittle">Master cuttin class</h6>
-            <div className="TabContent">
-              This is a test that we 're doing to check that everything is in
-              order. Greeting to the world{" "}
+            <h6 className="TabTittle">
+              How to make Mayonnnaise By Gordom Ramsay
+            </h6>
+            <div className="TabContent1">
+              Mayonnaise is said to be the invention of the French chef of the
+              Duke de Richelieu in 1756. While the duke was defeating the
+              British at Port Mahon, his chef was creating a victory feast that
+              included a sauce made of cream and eggs. When the chef realized
+              that there was no cream in the kitchen, he improvised,
+              substituting olive oil for the cream. A new culinary masterpiece
+              was born, and the chef named it 'Mahonnaise' in honour of the
+              Duke's victory. Today you will learn the best and easiest way to
+              make your own Mayonnnaise. Mayonnaise basically is a emulsion of
+              egg yokes and oil, is best if you use a neutral flavor oil like
+              sunflower seeds, or it can be a mix of oils that depends on each
+              person and taste. The ingredients can vary and it depends of you
+              taste. Come let me show you the technique.
             </div>
+            <div className="TabContent2"></div>
           </Tab>
           <Tab tabClassName="TabTab" eventKey="profile" title="Content List">
             <h6 className="TabTittle">Master cuttin class</h6>{" "}
             <ol className="TabContent">
+              <div className="tabcontenttittle">Mise En Place</div>
+              <div className="tabcontent">
+                In this Section we will be get all the ingredients ready before
+                we start cooking as it is. Let's start by washing all the
+                vegetables and peel the onions.
+              </div>
+
               <li>
-                <div className="SectionTittle">Mise En Place</div>
+                <div className="SectionTittle">List of ingredients</div>
                 <div className="SectionText">
-                  In this Section we will be get all the ingredients ready
-                  before we start cooking as it is. Let's start by washing all
-                  the vegetables and peel the onions.
+                  <ul>
+                    <li>3 egg yokes</li>
+                    <li> 200ml Neutral flavored oil (like canola)</li>
+                    <li> 1 teaspoon of English Mustard</li>
+                    <li> Juice from 1/2 lemon</li>
+                    <li> A pinch of salt</li>
+                    <li> A pinch of Black pepper </li>
+                  </ul>
                 </div>
               </li>
               <li>
-                <div className="SectionTittle">Onions</div>
+                <div className="SectionTittle">Let's talk about the oil</div>
                 <div className="SectionText">
-                  The onions should be peeled wiht a "puntilla" like show on the
-                  video. Tip/trick: if you put the onions on warm water for
-                  about 5 minutes, is gonna be easier to clean after.
+                  You might think that olive oil is better for a Mayonnaise, but
+                  you must think that the olive oil has a strong flavour that
+                  can have a overwelming flavour, that might be too strong for a
+                  regular Mayonnaise. The idea is to balance the flavor of the
+                  sauce by adding the right ingredients, taking in consideration
+                  your taste of food. But that doesn't mean that you cannot use
+                  it, if you want the olivve oil flavour in a mayo you can even
+                  mix different oils to have different taste, I personally would
+                  use olive oil a 20% and 80% neutral oil, to balance the
+                  flavour.
                 </div>
               </li>
               <li>
-                <div className="SectionTittle"></div>
+                <div className="SectionTittle">Let's talk about the eggs</div>
                 <div className="SectionText">
-                  In this Section we will be get all the ingredients ready
-                  before we start cooking as it is.
+                  You might not know the diference between white and brown eggs,
+                  it is interesting but must people don't know about it. The
+                  food a hen eats affects the yolk colour of her eggs. Generally
+                  speaking, if she eats a wheat-based diet, she lays eggs with
+                  pale yellow yolks. Feed that contains corn or alfalfa produces
+                  eggs with medium or darker yellow yolks. Aside from the colour
+                  of the eggshell, there is little difference between brown and
+                  white eggs. The eggshell colour depends on the breed of the
+                  hen. Generally speaking, white shell eggs come from hens with
+                  white feathers, while brown shell eggs are produced by hens
+                  with brown feathers. Nutritionally, both brown and white eggs
+                  are identical unless the feed has been enhanced for speciality
+                  eggs such as Omega-3.
                 </div>
               </li>
               <li>
-                <div className="SectionTittle"></div>
-                <div className="SectionText">
-                  In this Section we will be get all the ingredients ready
-                  before we start cooking as it is.
+                <div className="SectionTittle">
+                  Let's talk about English Mustart
                 </div>
-              </li>
-              <li>
-                <div className="SectionTittle"></div>
                 <div className="SectionText">
-                  In this Section we will be get all the ingredients ready
-                  before we start cooking as it is.
+                  English Mustard is quite hot. It comes either as a powder in a
+                  tin, or as a prepared mustard in a jar. If a recipe just says
+                  “English Mustard” or “mustard powder”, it's the powder that is
+                  meant. The powder is mostly used in the kitchen, the prepared
+                  is used at the table. The prepared is bright yellow from the
+                  turmeric in it.
                 </div>
               </li>
               <li>
--- src/front/js/pages/CreateCourse.js
+++ src/front/js/pages/CreateCourse.js
@@ -1,235 +1,50 @@
-import React from "react";
-import "../../styles/CreateCourse.css";
-import ClaseRemote from "../../img/claseRemote.jpg";
-import Form from "react-bootstrap/Form";
-import { Row, Col } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
+import React, { useState } from "react";
+import { Form, Input, Button, Radio } from "antd";
+
 export const CreateCourse = () => {
+  const [form] = Form.useForm();
+  const [requiredMark, setRequiredMarkType] = useState("optional");
+
+  const onRequiredTypeChange = ({ requiredMarkValue }) => {
+    setRequiredMarkType(requiredMarkValue);
+  };
+
   return (
-    <div className="BackGround">
-      <img src={ClaseRemote} className="HeaderCreate"></img>
-      <div className="wrapper container-fluid">
-        <div className="Tittle col">
-          <h3>Educator Center</h3>
-        </div>
-        <div className="Slogan col">
-          <h4>
-            Better than a thousand days of diligent study, is one day with a
-            great teacher.
-          </h4>
+    <div classname="Background">
+      <Form
+        form={form}
+        layout="vertical"
+        initialValues={{
+          requiredMarkValue: requiredMark,
+        }}
+        onValuesChange={onRequiredTypeChange}
+        requiredMark={requiredMark}
+      >
+        <Form.Item label="Required Mark" name="requiredMarkValue">
+          <Radio.Group>
+            <Radio.Button value="optional">Optional</Radio.Button>
+            <Radio.Button value>Required</Radio.Button>
+            <Radio.Button value={false}>Hidden</Radio.Button>
+          </Radio.Group>
+        </Form.Item>
 
-          <div className="Recomend col">
-            On this page we will got through the basics that we required in
-            order to post your course in our platform. Don’t worry is not
-            complicated:
-            <ul>
-              <li className="List row">
-                {" "}
-                <strong>
-                  Name and basic information about the course,
-                </strong>{" "}
-                you will create a Title and fill the form for basic information
-                about the course like a small description of your course, what
-                will you need for it, a brief description about yourself , etc.
-                This will be the first interaction with the final customer so
-                it's important to choose wisely your words.
-              </li>
-              <li>
-                <strong>Start creating your course,</strong> it is made in a way
-                that will make easy and fast to understand, in this section you
-                will write all your content step by step (divided in
-                sections)with definitions, recomendations, links to other source
-                of extra info, etc. Take in consideration that your objective is
-                to give the most quantity of high value information, describing
-                it in a way that everyone could understand.
-              </li>
-              <li>
-                <strong>Upload your Media Content,</strong> it will be displayed
-                acording to the step or chapter that you're in. the idea is to
-                explain yourself for what you wrote on the chapter. Include
-                (pdf, jpg, png, mp4, mp3)
-              </li>
-              <li>
-                <strong>Review and edit your course,</strong> reorganize,
-                delete, add, upload, format, colors, basic design, and other
-                tools that will help you to make the course appealing to the
-                user.
-              </li>
-            </ul>
-          </div>
-        </div>
+        <Form.Item label="Field A" required tooltip="This is a required field">
+          <Input placeholder="input placeholder" />
+        </Form.Item>
 
-        <div className="backform row">
-          <Form className="theform">
-            <Row className="mb-3">
-              <Form.Group
-                as={Col}
-                className="form-one row"
-                controlId="formGridEmail"
-              >
-                <Form.Label className="namecoursetittle col">
-                  Name The Course
-                </Form.Label>
-                <Form.Control
-                  className="namebar col"
-                  type="text"
-                  placeholder="For example: Burger's MasterChef Classes"
-                />
-              </Form.Group>
-              <Form.Group
-                className="describe-tittle mb-3 row"
-                controlId="exampleForm.ControlTextarea1"
-              >
-                <Form.Label>Describe your course</Form.Label>
+        <Form.Item
+          label="Field B"
+          tooltip={{
+            title: "Tooltip with customize icon",
+          }}
+        >
+          <Input placeholder="input placeholder" />
+        </Form.Item>
 
-                <Form.Control
-                  className="describe"
-                  type="text"
-                  as="textarea"
-                  rows={3}
-                  placeholder="Give a brief Description about what the Student is going to Learn in this course"
-                />
-              </Form.Group>
-              <Form.Select
-                className=" lvlcomplex row"
-                aria-label="Default select example"
-              >
-                <option>Level of complexity</option>
-                <option value="">easy</option>
-                <option value="">Medium</option>
-                <option value="">Hard</option>
-              </Form.Select>
-              <Form.Select aria-label="Default select example">
-                <option>Country</option>
-                <option value="">Spain</option>
-                <option value="">Venezuela</option>
-                <option value="Holanda">Holanda</option>
-              </Form.Select>
-              <Form.Select aria-label="Default select example">
-                <option>Style</option>
-                <option value="">Vegan Cuisine</option>
-                <option value="">Haute Cuisine</option>
-                <option value="">Nouvelle Cuisine</option>
-                <option value="">Fusion Cuisine</option>
-                <option value="">Vegetarian Cuisine</option>
-              </Form.Select>
-              <Form.Select aria-label="Default select example">
-                <option>Main Ingredient</option>
-                <option value="Spain">Fish and Seafood</option>
-                <option value="Spain">Pulse</option>
-                <option value="Spain">Meat and Poultry</option>
-                <option value="Venezuela">Vegetables</option>
-                <option value="Holanda">Dairy</option>
-              </Form.Select>
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>
-                  requirements needed from student to complete it
-                </Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Please add one by one from utensils like :pans, pots, tipe of knife,etc."
-                />{" "}
-                <span>Click here to add utensils requirements +</span>
-              </Form.Group>
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>
-                  knowledge and skills required for the student to complete
-                  succesfully the course..
-                </Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="For example: cutting lvl advance, wok, wood oven, etc. "
-                />{" "}
-                <span>
-                  Click here to add another required knowledge or skill +
-                </span>
-              </Form.Group>
-              <Row className="mb-3">
-                <Form.Group as={Col} controlId="formGridEmail">
-                  <Form.Label>Price</Form.Label>
-                  <Form.Control type="text" placeholder="Type the Price in €" />
-                </Form.Group>
-              </Row>{" "}
-              <h4>Section one</h4>
-              <h5>The course</h5>
-              <span>
-                Here you will be able to explain to the student how to get the
-                ingredients ready , how to cut it, etc. Explain to them how to
-                get the mise en place ready for cooking. So for the moment
-                please type all explanation. Down bellow you will be able to
-                upload your video!{" "}
-              </span>
-              <span>
-                First add a title, after choosing a title add the explanation
-                and Video.
-              </span>
-              <span>
-                <strong>recomendations</strong>
-                Please take in consideration that the more details, knowledge
-                and Tips you give the better for the student, making your course
-                more appealing.
-              </span>
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>Title</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="For example: Vegetables Stock"
-                />{" "}
-              </Form.Group>
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlTextarea1"
-              >
-                <Form.Label>Brief context</Form.Label>
-                <Form.Control
-                  as="textarea"
-                  rows={3}
-                  placeholder="I learned this recipe from one of the best chef i know personally and i want to share this recipe to the world"
-                />
-              </Form.Group>
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>List of ingredients</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="For example: Vegetables Stock"
-                />{" "}
-                <span>Click here to add ingredients +</span>
-              </Form.Group>
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>Step Title</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="For example Cutting the vegetables"
-                />{" "}
-              </Form.Group>
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlTextarea1"
-              >
-                <Form.Label>Step 1</Form.Label>
-                <Form.Control
-                  as="textarea"
-                  rows={3}
-                  placeholder="Onions: Cut the onions in mirepoix 5mm x 5mm."
-                />
-              </Form.Group>
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>Please add Video URL</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="https://www.youtube.com/watch?v=MUxtbsAUfp8"
-                />{" "}
-                <span>Click here to add another step +</span>
-              </Form.Group>
-              <a href="">Review Course</a>
-            </Row>
-
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </div>
-      </div>
+        <Form.Item>
+          <Button type="primary">Submit</Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 };
