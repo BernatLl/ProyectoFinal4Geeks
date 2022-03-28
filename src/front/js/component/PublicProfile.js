718398570da26b74dd48300a5029e30c09b6fce9
BernatLl <blloret@hotmail.com>
2022-03-28 07:57:37 +0000

    update cloudinary

--- src/front/js/component/PublicProfile.js
+++ src/front/js/component/PublicProfile.js
@@ -13,31 +13,27 @@ export const PublicProfile = () => {
 
   return (
     <>
-      
-      
-
       <div className="container">
-      <div className="titulo-profile">
-        <h1>Public profile</h1>
-      </div>
-        {store.user.map((select, i)=>(
+        <div className="titulo-profile">
+          <h1>Public profile</h1>
+        </div>
+        {store.user.map((select, i) => (
           <div className="container-fluid" key={i}>
             <Row>
               <Col className="col-6">
-                  <div   >
-                  <img src={select.image} className="fotoperfil"  />
-                  </div>
-              </Col> 
-              <Col className="col-6">   
-                  <div>
-                    <h4>Name: {select.full_name}</h4>
-                    <p>Description: {select.student_description}</p>
-                    <p>User Name: {select.username}</p>
-                    <p>Email: {select.email}</p>
-                  </div>
-              </Col>    
+                <div>
+                  <img src={select.image} className="fotoperfil" />
+                </div>
+              </Col>
+              <Col className="col-6">
+                <div>
+                  <h4>Name: {select.full_name}</h4>
+                  <p>Description: {select.student_description}</p>
+                  <p>User Name: {select.username}</p>
+                  <p>Email: {select.email}</p>
+                </div>
+              </Col>
             </Row>
-            
           </div>
         ))}
       </div>
